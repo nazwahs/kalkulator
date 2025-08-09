8aa12de7ccadeb5e7a153516caef7e49c64c7f86
nazwahs <waanazwa03@gmail.com>
2025-08-09 19:44:34 +0700

    first commit - upload project kalkulator

---
+++ kal.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def kalkulator():
+    ekspresi = request.form.get("ekspresi", "")
+    tombol = request.form.get("angka")
+    hasil = ""
+
+    if tombol == "C":
+        ekspresi = ""
+    elif tombol == "DEL":
+        ekspresi = ekspresi[:-1]  # hapus satu karakter terakhir
+    elif tombol == "=":
+        try:
+            hasil = str(eval(ekspresi))
+        except:
+            hasil = "ERROR"
+    elif tombol:
+        ekspresi += tombol
+
+    return render_template("index.html", ekspresi=ekspresi, hasil=hasil)
+
+if __name__ == "__main__":
+    app.run(debug=True)
